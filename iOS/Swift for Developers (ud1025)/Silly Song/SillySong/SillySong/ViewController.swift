5c698301f499f479b1427892d61f5c2e9f9d541d
Spyridion Georges Fotakos <s.fotakos@gmail.com>
2018-07-08 19:25:53 -0300

    Adds logic to displayLyrics

--- iOS/Swift for Developers (ud1025)/Silly Song/SillySong/SillySong/ViewController.swift
+++ iOS/Swift for Developers (ud1025)/Silly Song/SillySong/SillySong/ViewController.swift
@@ -29,5 +29,31 @@ class ViewController: UIViewController {
     }
     
     @IBAction func displayLyrics(_ sender: Any) {
+        let songLyrics = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+        lyricsView.text = songLyrics
     }
 }
+
+func shortNameFromName(name: String) -> String {
+    var lowerCaseName = name.folding(options: .diacriticInsensitive, locale: NSLocale.current).lowercased()
+    let vowelCharacterSet = CharacterSet.init(charactersIn: "aeiou")
+    for unicodeScalar in lowerCaseName.unicodeScalars {
+        if vowelCharacterSet.contains(UnicodeScalar(unicodeScalar.value)!) {
+            return String(lowerCaseName[lowerCaseName.index(of: Character(unicodeScalar))!...])
+        }
+    }
+    return name
+}
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    return lyricsTemplate
+        .replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortNameFromName(name: fullName))
+}
+

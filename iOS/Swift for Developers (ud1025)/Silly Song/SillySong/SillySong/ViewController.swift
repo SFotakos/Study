2afe63b8c55ced12c6b0e1f26318f363b57cf231
Spyridion Georges Fotakos <s.fotakos@gmail.com>
2018-07-08 18:37:56 -0300

    SillySong app initial commit

---
+++ iOS/Swift for Developers (ud1025)/Silly Song/SillySong/SillySong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SillySong
+//
+//  Created by Spyridion Georges Fotakos on 08/07/2018.
+//  Copyright © 2018 Spyridion Georges Fotakos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

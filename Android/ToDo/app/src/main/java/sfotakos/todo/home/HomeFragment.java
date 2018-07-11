83f4212bc15bdec3709b8f74bb48ef643de71ca7
Spyridion Georges Fotakos <s.fotakos@gmail.com>
2018-07-10 23:05:00 -0300

    Add static presenter for full test

--- Android/ToDo/app/src/main/java/sfotakos/todo/home/HomeFragment.java
+++ Android/ToDo/app/src/main/java/sfotakos/todo/home/HomeFragment.java
@@ -2,19 +2,26 @@ package sfotakos.todo.home;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import sfotakos.todo.R;
+import sfotakos.todo.Task;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment implements HomeView {
+
+    //TODO implement loader
+    static HomePresenterImpl homePresenter;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,6 +32,23 @@ public class HomeFragment extends Fragment implements HomeView {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        homePresenter = new HomePresenterImpl();
+        homePresenter.onViewAttached(this);
+
+        final EditText taskName = view.findViewById(R.id.taskName);
+        ImageButton addTask = view.findViewById(R.id.addTask);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Task task = new Task(1, taskName.getText().toString(), false);
+                homePresenter.add(task);
+            }
+        });
     }
 
     @Override
